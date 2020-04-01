53bdc7e11ececcf2a17d21cf4f10392a4446c0e2
nosahama <emmanuelnosa.e@gmail.com>
2020-04-01 02:06:04 +0200

    Initial blog structure

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+setTimeout(function(){
+        new Typed('.typed', {
+            strings: ["I am a Software Engineer ", "I code in Python, C#, Javascript, name it :D ", "I automate processes and build awesome stuff ", "Some Humour/Tech Pun ... ", "Beaches are filled with *Perls* or *Ruby* and grasses filled with *Pythons* ", "life is life", "it's all about how you *React*, but do not *Vue* life from an *Angular* perspective", "always make sure you *C#* and move *Swift*", "Well, Humour aside ", "I learn, I code, I debug, recursively ... ", "I am a Software Engineer "],
+            typeSpeed: 40,
+            backDelay: 1200,
+            smartBackspace: true,
+            showCursor: true,
+            loop: false,
+            loopCount: false,
+            cursorBlinking: true,
+            callback: function(){ }
+        });
+    }, 300);
